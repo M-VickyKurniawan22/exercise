84bb11db8c4f2b5c4b9267e3ba7e3a04adf1c805
Muhammad Vicky Kurniawan <muhammad.vickykurniawan@gmail.com>
2021-03-31 18:01:38 +0700

    Activity 4

--- app/src/main/java/com/example/activity3/MainActivity.java
+++ app/src/main/java/com/example/activity3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activity3;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,11 +19,27 @@ public class MainActivity extends AppCompatActivity {
     String nama, password;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(),ActivityDaftar.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogin = findViewById(R.id.btSignin);
 
+        btnLogin = findViewById(R.id.btSignin);
         edemail = findViewById(R.id.edEmail);
 
         edpasswords = findViewById(R.id.edPassword);
@@ -31,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onClick(View view) {
+
                 nama = edemail.getText().toString();
 
                 password = edpasswords.getText().toString();
